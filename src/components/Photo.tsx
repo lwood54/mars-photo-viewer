4aa6cd8f6604ec3979228b768b8031108ace6711
Logan Wood <lwood3499@gmail.com>
2020-10-23 23:15:22 -0500

    processing fetch

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,22 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { PhotoData } from "../types/interfaces";
 
+const api_key = "ho6NPUw7FBguQ4Nsju6c0NKaBS8x6joZjffiuxo6";
+const roverType = "curiosity";
+// const roverType = "opportunity";
+// const roverType = "spirit";
+
 function Photo() {
 	let [photoList, setPhotoList] = useState([]);
-	let [data, setData] = useState({});
-	let [sampImg, setSampImg] = useState("");
+
+	// TODO: create context that stores selected parameters
+	// temporarily create some state in here
+	// create 2 fetch functions, one for sol param and one for earth_date param
 
 	useEffect(() => {
-		fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=ho6NPUw7FBguQ4Nsju6c0NKaBS8x6joZjffiuxo6")
+		fetchPhotoData("all", 500, roverType);
+	}, []);
+
+	const fetchPhotoData = (camera: string, sol: number, roverType: string) => {
+		// can modify camera, sol, earth_date, and roverType
+		// fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${roverType}/photos?sol=1000&camera=chemcam&api_key=${api_key}`)
+		fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${roverType}/photos?earth_date=2015-6-3&camera=mast&api_key=${api_key}`)
 			.then((response) => response.json())
 			.then((data) => {
-				setData(data);
-				let shortList = data.photos.slice(0, 10);
-				console.log("shortList[0]", shortList[0]);
-				setPhotoList(shortList);
-				setSampImg(shortList[0].img_src);
+				console.log("data: ", data.photos);
+				// data will need to be a context as well to store this in the store
+				setPhotoList(data.photos);
 			});
-	}, []);
+	};
 
 	const photoEls = photoList.map((photo: PhotoData, i: number) => {
 		return <img src={photo.img_src} alt="" key={photo.id} />;
