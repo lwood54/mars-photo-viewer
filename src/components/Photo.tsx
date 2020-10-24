61b7cc108df2666eaf943fd84b9a125c6cfdb260
Logan Wood <lwood3499@gmail.com>
2020-10-23 23:25:09 -0500

    wip | fetch & process

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -20,7 +20,7 @@ function Photo() {
 	const fetchPhotoData = (camera: string, sol: number, roverType: string) => {
 		// can modify camera, sol, earth_date, and roverType
 		// fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${roverType}/photos?sol=1000&camera=chemcam&api_key=${api_key}`)
-		fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${roverType}/photos?earth_date=2015-6-3&camera=mast&api_key=${api_key}`)
+		fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${roverType}/photos?earth_date=2015-6-3&camera=rhaz&api_key=${api_key}`)
 			.then((response) => response.json())
 			.then((data) => {
 				console.log("data: ", data.photos);
